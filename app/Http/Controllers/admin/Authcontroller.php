0330b3e9b1cd3779f0b7e6b9791e1b86779dff5b
janani <jananik2104@gmail.com>
2026-03-26 15:14:43 +0530

    Fix AuthController class name

--- app/Http/Controllers/admin/Authcontroller.php
+++ app/Http/Controllers/admin/Authcontroller.php
@@ -8,47 +8,45 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 
-class Authcontroller extends Controller
+class AuthController extends Controller
 {
-   public function authenticate(Request $request){
-    $validator = Validator::make($request->all(), [
-        'email' => 'required|email',
-        'password' => 'required',
-    ]);
+    public function authenticate(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
 
-    if ($validator->fails()) {
-        return response()->json([
-            'status' => 400,
-            'error' => $validator->errors()
-        ], 400);
-    }
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 400,
+                'error' => $validator->errors()
+            ], 400);
+        }
 
-    if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
-               $user = User::find(Auth::user()->id);
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            $user = User::find(Auth::user()->id);
 
-               if($user->role == 'admin'){
+            if ($user->role == 'admin') {
+                $token = $user->createToken('token')->plainTextToken;
 
-               $token = $user->createToken('token')->plainTextToken;
-
-               return response()->json([
-            'status' => 200,
-            'token' => $token,
-            'id' => $user->id,
-            'name' => $user->name,
-        ],200);
-
-               }else{
                 return response()->json([
-            'status' => 401,
-            'message' => 'You are not authorized to access admin panel.'
-        ], 401);
-               }
-    }else{
-        return response()->json([
-            'status' => 401,
-            'message' => 'Either email/password is incorrect.'
-        ], 401);
+                    'status' => 200,
+                    'token' => $token,
+                    'id' => $user->id,
+                    'name' => $user->name,
+                ], 200);
+            } else {
+                return response()->json([
+                    'status' => 401,
+                    'message' => 'You are not authorized to access admin panel.'
+                ], 401);
+            }
+        } else {
+            return response()->json([
+                'status' => 401,
+                'message' => 'Either email/password is incorrect.'
+            ], 401);
+        }
     }
-
-   }
 }
